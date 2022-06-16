dd721843fa44dc3d652b27f6fb9bae79a37e1b97
Cory Pollard <astandard87@gmail.com>
2022-06-15 22:52:59 -0600

    Final upload. I mean it this time

--- script.js
+++ script.js
@@ -7,15 +7,23 @@
 
 */
 
+let clicked = false;
+
 const searchBtn = document.querySelector('.search__btn');
 const searchInput = document.querySelector('.search__input');
-const body = document.querySelector('body');
-
 searchBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-  searchInput.style.transform = 'scaleX(1)';
-  searchInput.focus();
-  body.addEventListener('click', function () {
-    searchInput.style.transform = 'scaleX(0)';
-  });
+  if (!clicked) {
+    e.preventDefault();
+    searchInput.style.transform = 'scaleX(1)';
+    searchInput.focus();
+    clicked = true;
+  }
+  if (clicked) {
+    searchBtn.addEventListener('click', function (e) {
+      e.preventDefault();
+      searchInput.style.transform = 'scaleX(0)';
+      searchInput.blur();
+      clicked = false;
+    });
+  }
 });
